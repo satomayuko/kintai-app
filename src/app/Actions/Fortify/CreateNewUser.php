a8dfe475d62358d83246826b5f4c8d7959c4003f
satomayuko <puru_zas13_9@yahoo.co.jp>
2026-01-23 21:14:40 +0900

    Update docker-compose and Fortify user creation

--- src/app/Actions/Fortify/CreateNewUser.php
+++ src/app/Actions/Fortify/CreateNewUser.php
@@ -18,18 +18,20 @@ class CreateNewUser implements CreatesNewUsers
                 'required',
                 'string',
                 'email',
+                'max:255',
                 Rule::unique(User::class),
             ],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ];
 
         $messages = [
-            'name.required' => '名前を入力してください',
-            'name.string' => '名前は文字で入力してください',
-            'name.max' => '名前は255字以下で入力してください',
+            'name.required' => 'お名前を入力してください',
+            'name.string' => 'お名前は文字で入力してください',
+            'name.max' => '名前は20字以下で入力してください',
             'email.required' => 'メールアドレスを入力してください',
             'email.string' => 'メールアドレスは文字で入力してください',
             'email.email' => 'メール形式で入力してください',
+            'email.max' => 'メールアドレスは255字以下で入力してください',
             'email.unique' => 'こちらのメールアドレスはすでに登録されています',
             'password.required' => 'パスワードを入力してください',
             'password.string' => 'パスワードは文字列で入力してください',
